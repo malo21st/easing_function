2527daf248a33737340c7c94bc747402e7e636c5
malo21st <44739330+malo21st@users.noreply.github.com>
2022-12-29 00:35:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,14 @@ st.dataframe(df)
 num = 40
 st.image(f"./gif/{df.iloc[num, 1]}", format = 'GIF')
 st.code(f"{df.iloc[num, 2]}", language="python")
-st.image("./gif/easing_light.gif", format = 'GIF')
+st.image(, format = 'GIF')
+
+"""### gif from local file"""
+with open("./gif/easing_light.gif", "rb") as file_ :
+  contents = file_.read()
+  data_url = base64.b64encode(contents).decode("utf-8")
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
