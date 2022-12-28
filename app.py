5b0df6d7f8a0ee47b97f58ba28b88d8d4ef74294
malo21st <44739330+malo21st@users.noreply.github.com>
2022-12-29 00:27:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ df = pd.read_json("easing.json")
 st.dataframe(df)
 
 num = 40
-st.image(f"./gif/{df.iloc[num, 1]}")
+st.image(f"./gif/{df.iloc[num, 1]}", format = 'GIF')
 st.code(f"{df.iloc[num, 2]}", language="python")
-st.image("./gif/easing_light.gif")
+st.image("./gif/easing_light.gif", format = 'GIF')
